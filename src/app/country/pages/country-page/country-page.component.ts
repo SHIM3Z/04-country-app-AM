758219bd08efdf90d2725a58aed0e12a1b00e191
SHIM3Z <shim3z151002@gmail.com>
2025-03-25 22:54:50 -0500

    fin seccion 9

---
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'app-country-page',
+  imports: [],
+  templateUrl: './country-page.component.html',
+})
+export class CountryPageComponent {
+  country = toSignal(
+    inject(ActivatedRoute).params.pipe(map((params) => params['code']))
+  );
+}
